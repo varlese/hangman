a4ba4a7ebee762abf414f833f7c8275029d689e9
varlese <1361760+varlese@users.noreply.github.com>
2019-10-28 16:54:47 -0400

    Adding function to guess word, style for guesses, and some refactoring.

--- hangman.py
+++ hangman.py
@@ -10,25 +10,34 @@ words = ["brazil",
          "suitcase"]
 
 def selectWord():
-    word = random.choice(words);
+    word = random.choice(words)
     return(word)
 
-def getWordLength():
+def playGame():
     word = selectWord()
-    wordLength = len(word)
-    return wordLength
-
-def guessWord():
-    word = selectWord()
-    attempts = getWordLength() + 2
-    guesses = []
+    attempts = len(word) + 2
+    guessList = []
     for attempt in range(attempts):
-        char = input("Guess a letter. \n")
+        char = input("Guess a letter. \n").lower()
         if char in word:
             print(f"Correct! {char} is in the word. ")
-            guesses.append(char)
-            print(f"Your correct guesses so far are {guesses}.")
+            guessList.append(char)
+            guesses = ' '.join(map(str, guessList))
+            print(f"Your correct guesses so far are: {guesses}.")
         else:
             print("Wrong!")
+            print(f"Your correct guesses so far are: {guesses}.")
+    guessWord = input("Would you like to guess the word? \n").lower()
+    if (guessWord == word):
+        print("Correct, you win! \n")
+        score += 1
+    else:
+        print("You lose.")
+    rematch = input("Would you like to play again? y/n \n").lower()
+    if (rematch == "y"):
+        playGame()
+    else:
+        print("Okay, goodbye.")
+        return
 
-guessWord()
+playGame()
