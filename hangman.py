22fcb4c3c0486de012eff6f1e8262ca6c9db6011
varlese <1361760+varlese@users.noreply.github.com>
2019-10-29 17:32:48 -0400

    Added validation and allows players to guess the word once they have all the letters correct.

--- hangman.py
+++ hangman.py
@@ -17,20 +17,25 @@ def playGame():
     word = selectWord()
     attempts = len(word) + 2
     guessList = []
+    guesses = " "
     for attempt in range(attempts):
         char = input("Guess a letter. \n").lower()
-        if char in word:
-            print(f"Correct! {char} is in the word. ")
-            guessList.append(char)
-            guesses = ' '.join(map(str, guessList))
-            print(f"Your correct guesses so far are: {guesses}.")
+        if char.isalpha():
+            if char in word:
+                guessList.append(char)
+                guesses = ' '.join(map(str, guessList))
+                print(f"Correct! {char} is in the word. " +
+                      f"Your correct guesses so far are: {guesses}.")
+                if len(word) == len(guessList):
+                    print(f"You\'ve guessed all of the letters!")
+                    break
+            else:
+                print("Wrong!")
         else:
-            print("Wrong!")
-            print(f"Your correct guesses so far are: {guesses}.")
-    guessWord = input("Would you like to guess the word? \n").lower()
+            print(f'That\'s not a letter! Try again.')
+    guessWord = input(f"Would you like to guess the word? \n").lower()
     if (guessWord == word):
-        print("Correct, you win! \n")
-        score += 1
+        print("Correct, you win!")
     else:
         print("You lose.")
     rematch = input("Would you like to play again? y/n \n").lower()
